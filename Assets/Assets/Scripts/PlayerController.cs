e76ed1d9e51336bf84934e6e0545b45fded81229
beefydude <malkin.dmytro@gmail.com>
2023-12-31 19:28:42 +0100

    basic movement & jump for character

--- Assets/Assets/Scripts/PlayerController.cs
+++ Assets/Assets/Scripts/PlayerController.cs
@@ -6,8 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public float jumpForce;
+    public float gravityScale = 5f;
 
     private Vector3 moveDirection;
+
+    public CharacterController characterController;
 
     // Start is called before the first frame update
     void Start()
@@ -18,13 +21,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float yStore = moveDirection.y;
         moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveDirection *= moveSpeed;
+        moveDirection.y = yStore;
 
         if (Input.GetButtonDown("Jump"))
         {
             moveDirection.y = jumpForce;
         }
 
-        transform.position += moveDirection * Time.deltaTime * moveSpeed;
+        moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
+
+        // transform.position += moveDirection * Time.deltaTime * moveSpeed;
+
+        characterController.Move(moveDirection * Time.deltaTime);
+
     }
 }
